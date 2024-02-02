a75827f0611171c4e655d0a7cf840ae524546b7b
Martin Flischman <hem0inn@icloud.com>
2024-02-02 18:26:35 +0200

    Gif images
    feat: add gif images that is displayed with either rsvp selection

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Get the form element and confirmation message element
-const rsvpForm = document.getElementById('rsvp-form')
-const confirmationMessage = document.getElementById('confirmation-message')
+const rsvpForm = document.getElementById('rsvp-form');
+const confirmationMessage = document.getElementById('confirmation-message');
+const body = document.body;
+
+// Function to set background image
+function setBackgroundImage(url) {
+  document.body.style.backgroundImage = url ? `url("${url}")` : 'none';
+}
 
 // Add event listener to the form submission
 rsvpForm.addEventListener('submit', (event) => {
-  event.preventDefault() // Prevent form submission
+  event.preventDefault(); // Prevent form submission
 
   // Get the selected attendance value
-  const attendance = document.getElementById('attendance').value
+  const attendance = document.getElementById('attendance').value;
 
   // Display confirmation message based on attendance selection
   if (attendance === 'yes') {
-    confirmationMessage.innerHTML = 'Party on! We look forward to seeing you at the GIF Gala! 🎉'
-  } else if (attendance === 'no') {
-    confirmationMessage.innerHTML = `We're sorry to hear you can't make it. We hope to see you at the next event!`
+    confirmationMessage.innerHTML = '🎉 Party on! We look forward to seeing you at the GIF Gala!';
+    body.style.backgroundImage = 'url("https://media.giphy.com/media/bZQuYd8U3Vqfe/giphy.gif")'
+  } else {
+    confirmationMessage.innerHTML = `😔 We're sorry that you can't make it. We hope to see you at the next event!`;
+    body.style.backgroundImage = 'url("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWFtenZ0ejNkdzI5cDE4MjB4amY3OTViaDJvMTN4NTc4aGFjeXkxcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11xVhnKOKtAj5e/giphy.gif")'
   }
-  
+
   // Show the confirmation message
-  confirmationMessage.style.display = 'block'
-  
+  confirmationMessage.style.display = 'block';
+
   // Reset the form
-  rsvpForm.reset()
-})
+  rsvpForm.reset();
+});
